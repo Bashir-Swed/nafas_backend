b97e27f4dffbeca54f8db049c5578cd163066c0d
shaima-7 <shsw7448@gmail.com>
2025-12-03 12:07:47 +0300

    #

--- Nafas.DAL/Repositories/MedicalRepo.cs
+++ Nafas.DAL/Repositories/MedicalRepo.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using Nafas.DAL.DTOs.Diseases;
 using Nafas.DAL.DTOs.Medical;
 using System;
 using System.Collections.Generic;
@@ -37,6 +38,36 @@ namespace Nafas.DAL.Repositories
             }
             return false;
         }
+        public bool AddNewDisease(MedicalKnowledge Disease)
+        {
+            string query = @"insert into Diseases(DiseaseName,Discription,Prevention,Symptoms,Treatment)
+values(@DiseaseName,@Discription,@Prevention,@Symptoms,@Treatment)";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Global.connectionstring))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@DiseaseName",Disease.DiseaseName);
+                        command.Parameters.AddWithValue("@Discription", Disease.Discription);
+                        command.Parameters.AddWithValue("@Prevention", Disease.Prevention);
+                        command.Parameters.AddWithValue("@Symptoms", Disease.Symptoms);
+                        command.Parameters.AddWithValue("@Treatment", Disease.Treatment);
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return true;
+                        }
+                    }
 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
     }
 }
